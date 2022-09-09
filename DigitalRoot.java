beaf6655634e0509a66808da9e12a1ffaeffbc6e
kamil.hello <kamil.mymail@protonmail.com>
2022-09-10 01:19:03 +0200

    Shorter and better method

--- DigitalRoot.java
+++ DigitalRoot.java
@@ -1,20 +1,14 @@
 public class DigitalRoot {
 
   public static int digital_root(int n) {
-    if (n < 0)
-      throw new IllegalArgumentException();
 
-    if(n/10 != 0) //If more than one digit
-        {
-            int result =  digital_root(n/10) + n%10;
-
-            if(result/10 != 0) //If more than one digit
-                return digital_root(result);
-            else
-                return result;
-        }
-    else
+    if(n >= 10)     //If more than one digit
+        return digital_root(digital_root(n/10) + n%10);
+    else if(n >= 0) //If one digit
         return n;
+    else
+        throw new IllegalArgumentException();
+        
   }
   
 }
