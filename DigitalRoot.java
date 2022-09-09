fb0f38a661f5cb54229c9210a001048e081a2258
kamil.hello <kamil.mymail@protonmail.com>
2022-09-09 23:13:11 +0200

    Change result declaration

--- DigitalRoot.java
+++ DigitalRoot.java
@@ -4,12 +4,11 @@ public class DigitalRoot {
     if (n < 0)
       throw new IllegalArgumentException();
 
-    int result;
     if(n/10 != 0) //If more than one digit
         {
-            result =  digital_root(n/10) + n%10;
+            int result =  digital_root(n/10) + n%10;
 
-            if(result/10 !=0) //If more than one digit
+            if(result/10 != 0) //If more than one digit
                 return digital_root(result);
             else
                 return result;
